d2b9218c888d5cab9dab0bfbc5cff8f2ec2499f4
eliotrhys <eliot@eliotshort.co.uk>
2017-10-31 19:17:17 +0000

    Up to 10 now yeaa boi

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -21,11 +21,28 @@ end
 def length_of_string(test_string)
   test_string.length
 end
-#
-# def join_string
-#
-# end
+
+def join_string(string_1, string_2)
+  string_1 + string_2
+
+end
 
 def add_string_as_number(param1, param2)
   param1.to_i + param2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  end
+end
+
+def number_to_short_month_name(number)
+  cabbage = number_to_full_month_name(number)
+  return cabbage[0..2]
 end
